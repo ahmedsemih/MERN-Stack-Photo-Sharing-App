dc70675e6ac6fb29ac20906dadbb0196de06ef82
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-06-06 12:41:25 +0300

    Category model created

--- Api/server.js
+++ Api/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const photoRoutes = require('./routes/photoRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = 4000;
@@ -8,7 +13,14 @@ const port = 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+// ROUTES
+app.use('/photos', photoRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/users', userRoutes);
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(console.log("Successfully connected to database"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

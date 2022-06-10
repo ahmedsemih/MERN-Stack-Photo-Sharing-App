fb514d7b207858c9ab0d701719dca8519c6f6693
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-06-10 12:55:26 +0300

    Sign up and login validations added

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Typography, Box, Button, TextField, InputAdornment, InputLabel, FormControl, IconButton, OutlinedInput } from '@mui/material';
 import { VisibilityOff, Visibility } from '@mui/icons-material';
+import { useFormik } from 'formik';
+import LoginValidations from '../validations/LoginValidations';
 
 function LoginModal() {
 
@@ -9,43 +11,65 @@ function LoginModal() {
         setShowPassword(!showPassword);
     };
 
-  return (
-    <Box component='form' sx={{backgroundColor:'white', display: 'flex', justifyContent: 'center', boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px', flexDirection: 'column', width: { xs: '100vw', sm: '70vw', md: '50vw', lg: '30vw' } }}>
-                <Box sx={{backgroundColor:'#e61605', height:'1rem', mb:5}}></Box>
-        <Typography variant='h2' sx={{m:1,textAlign:'center'}}>Login</Typography>
-                <TextField 
-                    sx={{mx:{xs:1,md:5},my:2 }}
-                    label="Email"
-                    type="email"
-                    name='email'
+    const { values, handleSubmit, handleChange, resetForm, isValid } = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            console.log(values.email, values.password);
+            resetForm();
+        },
+        validationSchema: LoginValidations,
+    });
+
+    return (
+        <Box component='form' sx={{ backgroundColor: 'white', display: 'flex', justifyContent: 'center', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', flexDirection: 'column', width: { xs: '100vw', sm: '70vw', md: '50vw', lg: '30vw' } }}>
+            <Box sx={{ backgroundColor: '#e61605', height: '1rem', mb: 5 }}></Box>
+            <Typography variant='h2' sx={{ m: 1, textAlign: 'center' }}>Login</Typography>
+            <TextField
+                sx={{ mx: { xs: 1, md: 5 }, my: 2 }}
+                label="Email"
+                type="email"
+                name='email'
+                value={values.email}
+                onChange={handleChange}
+            />
+            <FormControl sx={{ mx: { xs: 1, md: 5 }, my: 2 }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    name='password'
+                    value={values.password}
+                    onChange={handleChange}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                onClick={handleClickShowPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
                 />
-                <FormControl sx={{mx:{xs:1,md:5},my:2}} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        name='password'
-                        type={showPassword ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    onClick={handleClickShowPassword}
-                                    edge="end"
-                                >
-                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Password"
-                    />
-                </FormControl>
-                <Box sx={{display:'flex', justifyContent:'center'}}>
-                    <Button sx={{ mt: 1 }} variant="contained" color="error" type='submit'>
-                        Login
-                    </Button>
-                </Box>
-                <Box sx={{backgroundColor:'#e61605', height:'1rem', mt:5}}></Box>
+            </FormControl>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Button
+                    sx={{ mt: 1 }}
+                    variant="contained"
+                    color="error"
+                    type='submit'
+                    onClick={handleSubmit}
+                    disabled={!isValid}>
+                    Login
+                </Button>
             </Box>
-  )
+            <Box sx={{ backgroundColor: '#e61605', height: '1rem', mt: 5 }}></Box>
+        </Box>
+    )
 }
 
 export default LoginModal
--- frontend/src/components/SignupModal.js
+++ frontend/src/components/SignupModal.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
-import { Typography, Box, Button, TextField, InputAdornment, InputLabel, FormControl, IconButton, OutlinedInput, FormControlLabel, Checkbox } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Box, Button, TextField, InputAdornment, InputLabel, FormControl, IconButton, OutlinedInput, FormControlLabel, Checkbox, FormHelperText } from '@mui/material';
 import { VisibilityOff, Visibility } from '@mui/icons-material';
+import { useFormik } from 'formik';
+import SignupValidations from '../validations/SignupValidations';
 
 
 function SignupModal() {
@@ -10,50 +12,84 @@ function SignupModal() {
         setShowPassword(!showPassword);
     };
 
-  return (
-    <Box component='form' sx={{backgroundColor:'white', display: 'flex', justifyContent: 'center', boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px', flexDirection: 'column', width: { xs: '100vw', sm: '70vw', md: '50vw', lg: '30vw' } }}>
-                <Box sx={{backgroundColor:'#e61605', height:'1rem', mb:5}}></Box>
-        <Typography variant='h2' sx={{m:1,textAlign:'center'}}>Sign Up</Typography>
-                <TextField
-                    sx={{ mx:{xs:1,md:5},my:2}}
-                    label="Email"
-                    type="email"
-                    name='email'
+    const { handleChange, handleBlur, errors, touched, isValid, values, handleSubmit, resetForm } = useFormik({
+        initialValues: {
+            email: '',
+            username: '',
+            password: '',
+            terms: false
+        },
+        onSubmit: values => {
+            console.log(values.username);
+            resetForm();
+        },
+        validationSchema: SignupValidations
+    })
+
+    return (
+        <Box component='form' sx={{ backgroundColor: 'white', display: 'flex', justifyContent: 'center', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', flexDirection: 'column', width: { xs: '100vw', sm: '70vw', md: '50vw', lg: '30vw' } }}>
+            <Box sx={{ backgroundColor: '#e61605', height: '1rem', mb: 5 }}></Box>
+            <Typography variant='h2' sx={{ m: 1, textAlign: 'center' }}>Sign Up</Typography>
+            <TextField
+                sx={{ mx: { xs: 1, md: 5 }, my: 2 }}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.email && errors.email}
+                helperText={touched.email && errors.email}
+                label="Email"
+                type="email"
+                name='email'
+                value={values.email}
+            />
+            <TextField
+                sx={{ mx: { xs: 1, md: 5 }, my: 2 }}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.username && errors.username}
+                helperText={touched.username && errors.username}
+                label="Username"
+                type="text"
+                name='username'
+                value={values.username}
+            />
+            <FormControl sx={{ mx: { xs: 1, md: 5 }, my: 2 }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && errors.password}
+                    name='password'
+                    value={values.password}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                onClick={handleClickShowPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
                 />
-                <TextField
-                    sx={{ mx:{xs:1,md:5},my:2}}
-                    label="Username"
-                    type="text"
-                    name='username'
-                />
-                <FormControl sx={{ mx:{xs:1,md:5},my:2}} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        name='password'
-                        type={showPassword ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    onClick={handleClickShowPassword}
-                                    edge="end"
-                                >
-                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Password"
-                    />
-                </FormControl>
-                <Box sx={{display:'flex', justifyContent:'space-between',mx:{xs:1,md:5},my:2}}>
-                <FormControlLabel control={<Checkbox defaultChecked />} name='agree' label="I agree the Terms of Service and Privacy Policy." />
-                    <Button sx={{ mt: 1 }} variant="contained" color="error" type='submit'>
-                        Sign Up
-                    </Button>
-                </Box>
-                <Box sx={{backgroundColor:'#e61605', height:'1rem', mt:5}}></Box>
+                {touched.password && <FormHelperText sx={{ color: '#e61605' }} >{errors.password}</FormHelperText>}
+            </FormControl>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mx: { xs: 1, md: 5 }, my: 2 }}>
+                <FormControlLabel
+                    control={<Checkbox checked={values.terms} />}
+                    onChange={handleChange}
+                    value={values.terms}
+                    name='terms'
+                    label="I agree the Terms of Service and Privacy Policy." />
+                <Button sx={{ mt: 1 }} variant="contained" color="error" disabled={!isValid} type='submit' onSubmit={handleSubmit} >
+                    Sign Up
+                </Button>
             </Box>
-  )
+            <Box sx={{ backgroundColor: '#e61605', height: '1rem', mt: 5 }}></Box>
+        </Box>
+    )
 }
 
 export default SignupModal;
